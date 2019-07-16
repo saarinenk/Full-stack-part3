2305cb533babd3de181b4612fdaf97e74ff93d9d
Katri Saarinen <katri.saarinen@kaikuhealth.com>
2019-07-16 14:47:53 +0300

    removed build folder from gitignore

---
+++ build/precache-manifest.33d4614806728f30d994e3cfb00c9b7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bcf2fe7abb9205aa4c91819d2a95ed4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "621cf4eda28a83f216a7",
+    "url": "/static/css/main.16178b8b.chunk.css"
+  },
+  {
+    "revision": "ba9a638464cbb67f01c7",
+    "url": "/static/js/2.db7ee473.chunk.js"
+  },
+  {
+    "revision": "621cf4eda28a83f216a7",
+    "url": "/static/js/main.fb6cef71.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
