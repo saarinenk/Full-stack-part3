e8634bf49ccd16305c2ba21dd146e71f151bd1e0
Katri Saarinen <katri.saarinen@kaikuhealth.com>
2019-07-16 13:57:31 +0300

    created heroku app

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
@@ -113,7 +116,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
