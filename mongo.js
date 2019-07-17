84d7d040ab2b2708856798782b8568fb31b583f4
Katri Saarinen <katri.saarinen@kaikuhealth.com>
2019-07-17 09:14:23 +0300

    ex 3.12

---
+++ mongo.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+const password = process.argv[2];
+
+const url = `mongodb+srv://saarinenk:${password}@cluster0-fullstack-rbnel.mongodb.net/phonebook?retryWrites=true&w=majority`;
+
+mongoose.connect(url, { useNewUrlParser: true });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+const person = new Person({
+  name: process.argv[3],
+  number: process.argv[4]
+});
+
+if (process.argv.length === 5) {
+  person.save().then(res => {
+    console.log(
+      `Added ${res.name} with number ${res.number} to the phonebook!`
+    );
+    mongoose.connection.close();
+  });
+} else if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log("Phonebook:");
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`);
+    });
+    mongoose.connection.close();
+  });
+}
